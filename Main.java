c8b2fa614da03a77c6b46a48458bb4e8c7f8a137
Felipe <felipemoreira1@gmail.com>
2022-01-11 22:50:40 -0300

    Stage 2/5: The user is the gamemaster

--- Main.java
+++ Main.java
@@ -13,12 +13,11 @@ public class Main {
     public static void printGrid(String input) {
 
         System.out.println("---------");
-        char[] grid = input.toCharArray();
         int pos = 0;
         for(int i = 0; i < 3; i++){
             System.out.print("| ");
             for(int j = 0; j < 3; j++){
-                System.out.print(grid[pos] + " ");
+                System.out.print(input.charAt(pos) + " ");
                 pos++;
             }
             System.out.println("|");

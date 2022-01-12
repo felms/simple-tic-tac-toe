529d6bafe5e60b0c32dc7d5f5d2c96771003230b
Felipe <felipemoreira1@gmail.com>
2022-01-11 22:43:31 -0300

    Stage 2/5: The user is the gamemaster

--- Main.java
+++ Main.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+        printGrid(input);
+    }
+
+    public static void printGrid(String input) {
+
+        System.out.println("---------");
+        char[] grid = input.toCharArray();
+        int pos = 0;
+        for(int i = 0; i < 3; i++){
+            System.out.print("| ");
+            for(int j = 0; j < 3; j++){
+                System.out.print(grid[pos] + " ");
+                pos++;
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
 }
